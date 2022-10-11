e86575d478cd1b9b466620719e8bf532c2163da2
kingkor399 <farhanrahmanyou@gmail.com>
2022-10-11 22:29:15 +0600

    seventh commit

--- src/Component/Options.jsx
+++ src/Component/Options.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Options = ({option}) => {
+const Options = ({option,optionBtn}) => {
     return (
         <div>
             <ul>
-                <li className='grid grid-cols-1 font-semibold mt-4 hover:bg-green-400 cursor-pointer w-96 border-solid border-2 border-indigo-200 p-4 rounded-lg'>{option}</li>
+                <li onClick={() => optionBtn(option)} className='grid grid-cols-1 font-semibold mt-4 hover:bg-green-400 cursor-pointer w-96 border-solid border-2 border-indigo-200 p-4 rounded-lg'>{option}</li>
             </ul>
         </div>
     );
--- src/Component/Quizees .js
+++ src/Component/Quizees .js
@@ -2,8 +2,15 @@ import React from 'react';
 import Options from './Options';
 
 const Quizees = ({ quize }) => {
-    console.log(quize)
     const { options, question, correctAnswer } = quize;
+    const optionBtn = (option) =>{
+        if(option === correctAnswer){
+            alert('write')
+        }
+        else{
+            alert('wrong')
+        }
+    }
     return (
         <div className="container rounded-lg bg-gray-100 mb-10 mt-4 shadow-md w-3/6 flex flex-col items-center mx-auto text-center lg:pb-2 md:py-10 md:px-10 lg:px-10">
             <div className=''>
@@ -13,6 +20,7 @@ const Quizees = ({ quize }) => {
                 {
                     options.map((option) => <Options
                         option={option}
+                        optionBtn = {optionBtn}
                     ></Options>)
                 }
             </div>
